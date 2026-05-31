446c776df812017eb0961ab121fb27663f119ec4
ANN MARIA <ann2004maria@gmail.com>
2026-05-31 21:35:39 +0530

    Add CORS middleware to FastAPI application

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,19 @@
 
 from fastapi import FastAPI
 from pymongo import MongoClient
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 MONGO_URI = os.getenv("MONGO_URI")
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 client = MongoClient(MONGO_URI)
 
